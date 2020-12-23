2cced0cbae5a33b5c8a592e1c2876bcaa7b2bebb
yelirkram <m@rkriley.com>
2020-12-23 21:05:16 +0000

    Bug fix

--- main.jl
+++ main.jl
@@ -4,7 +4,7 @@ my_name = chomp(readline())
 
 # Create a list of activities for T255 and store it in a variable
 # The [] combines values into a list
-activities = ["camping", "hiking", "canoeing", "high adventure", "rock climbing", "caving", "biking", "community service", "mountain climbing", "skiing", "swimming", "sailing"]
+activities = ["camping", "hiking", "canoeing", "high adventure", "rock climbing", "caving", "biking", "community service"]
 
 # Store the length of the activities list in another variable
 activities_count = length(activities)
@@ -15,28 +15,25 @@ answer = "n"
 
 # Create an empty string for storing the Scout's
 # favorite activity
-favorite_activity1 = ""
-favorite_activity2 = ""
+favorite_activity = ""
 
 # Loop until the user answers "y" to the question if this is his/her
 # favorite activity
 while answer == "n"
   # Get a random number between 1 and the length of the activities
   # variable and store it in a new variable
-  random_activity1 = rand(1:activities_count)
-  random_activity2 = rand(1:activities_count)
+  random_activity = rand(1:activities_count)
 
   # Get the name of the activity from the activities variable that
   # is stored in the location/index of the random number we just 
   # generated
-  global favorite_activity1 = activities[random_activity1]
-  global favorite_activity2 = activities[random_activity2]
+  global favorite_activity = activities[random_activity]
 
   # Ask the user is this is his/her favorite activity
   # and store the response back in the answer variable.
   # If answer == anything but n the it will break
   # out of the while loop
-  print("\nAre " * favorite_activity1 * " and " * favorite_activity2 * " your favorite Troop 255 activities " * my_name * "? (y/n)  ")
+  print("\nIs " * favorite_activity * " your favorite Troop 255 activity " * my_name * "? (y/n)  ")
 
   # Read the user's response and loop back up to the while
   global answer = chomp(readline())
@@ -44,7 +41,7 @@ while answer == "n"
 end # while answer == "n"
 
 # We have broken out of the loop, display a cheesy msg
-println("\nWell, " * my_name * ", " * favorite_activity1 * " and " * favorite_activity2 * " are my favorites too!")
+println("\nWell, " * my_name * ", " * favorite_activity * " is my favorite too!")
 
 # Suggestions to change
 # 1. Add or change the activities in the list
